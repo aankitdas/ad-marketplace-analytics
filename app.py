ee640922d53af4e86ac8027f0f4c49e60534b822
aankitdas <aankitdas5@gmail.com>
2026-01-08 20:40:12 -0600

    fixed app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import sys
+import os, sys
 sys.path.append('src')
 
 st.set_page_config(
